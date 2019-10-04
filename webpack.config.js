dd4959fc95d88c353508f08708118981d5db1746
dkodjabashev <dkodjabashev@liquidnet.com>
2019-10-04 10:42:46 +0100

    optimised build a bit and changed compile target to es2017

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,73 @@
-switch (process.env.NODE_ENV){
-  case 'prod':
-  case 'production':
-    module.exports = require('./config/webpack.prod');
-    break;
-}
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+
+const __OUTPUT__ = path.join(__dirname, 'dist');
+const __INPUT__ = path.join(__dirname, 'src');
+
+module.exports = {
+
+  devtool: 'source-map',
+
+  context: __dirname,
+
+  entry: {
+    index: [
+      path.join(__INPUT__, 'index.js')
+    ]
+  },
+
+  output: {
+    path: __OUTPUT__,
+    publicPath: '/',
+    filename: `slick.min.js`,
+    libraryTarget: 'commonjs2'
+  },
+
+  externals: [nodeExternals()],
+
+  module: {
+    loaders: [{
+      test: /\.(js)$/,
+      exclude: /(node_modules)/,
+      loader: 'babel',
+      query: {
+        presets: ['latest']
+      }
+    }, {
+      test: /\.(scss)$/,
+      loader: 'style-loader!scss-loader'
+    }]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false, // prod
+      mangle: {
+        screw_ie8: true
+      }, // prod
+      compress: {
+        screw_ie8: true
+      }, // prod
+      comments: false // prod
+    }),
+    new CopyWebpackPlugin([
+      {
+        flatten: true,
+        from: `${__INPUT__}/*.scss`
+      }
+    ])
+  ],
+
+  resolve: {
+    extensions: ['', '.js']
+  }
+};
